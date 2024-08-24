7a2e2720c450632bb276a3c8d4bd9590585529a7
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-24 21:18:39 +0500

    feat: add user in header and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styles from './page.module.css';
+import { auth } from '@/firebase/config';
 
 export default function Home() {
+  const [user] = useAuthState(auth);
+
   return (
     <main className={styles.main}>
-      <h2>Welcome</h2>
+      <h2>Welcome{user && ` Back, ${user?.displayName}`}!</h2>
     </main>
   );
 }
