b8694af5f3beb8aa1741d5792ef6fa574ce4a329
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-08-25 20:35:55 +0500

    feat: add FormField component

---
+++ src/components/FormField/types.ts
@@ -0,0 +1,11 @@
+// import { SignInFormData, SignUpFormData } from '@/types&interfaces/types';
+import { Control, FieldErrors, FieldValues } from 'react-hook-form';
+
+export type FormFieldProps<T extends FieldValues> = {
+  name: keyof T;
+  control: Control<T>;
+  label: string;
+  type?: string;
+  required?: boolean;
+  errors: FieldErrors<T>;
+};
