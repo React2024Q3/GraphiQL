d6e6f65579ae1429630f4865ebf135236e9462ee
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-01 18:47:04 +0500

    feat: change position for Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import { LinearProgress } from '@mui/material';
 
+import styles from './Loader.module.css';
+
 export const Loader: FC = () => {
-  return <LinearProgress />;
+  return <LinearProgress className={styles.loader} />;
 };
