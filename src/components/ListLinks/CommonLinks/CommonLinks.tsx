9431cc6a453c7456c068dcc11bd58e4bdf2690ee
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-01 18:46:14 +0500

    refactor: separate CommonLinks from ListLinks

---
+++ src/components/ListLinks/CommonLinks/CommonLinks.tsx
@@ -0,0 +1,20 @@
+import { Link } from '@/navigation';
+
+import styles from '../ListLinks.module.css';
+
+export function CommonLinks() {
+  return (
+    <>
+      <li className={styles.item}>
+        <Link className={styles.link} href='/rest'>
+          RESTful Client
+        </Link>
+      </li>
+      <li className={styles.item}>
+        <Link className={styles.link} href='/graphiql'>
+          GraphiQL Client
+        </Link>
+      </li>
+    </>
+  );
+}
---
+++ src/components/ListLinks/CommonLinks/index.ts
@@ -0,0 +1,1 @@
+export { CommonLinks } from './CommonLinks';
