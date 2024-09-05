c5ba989ae36cba6700d6cb391fe4f8ff47316bac
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-05 20:54:48 +0500

    feat: add photo

--- src/shared/constants/developersInfo.ts
+++ src/shared/constants/developersInfo.ts
@@ -1,24 +1,18 @@
 export const DEVELOPERS = [
   {
-    name: 'Oleksii Bolotin',
-    role: 'Team lead, Frontend developer',
     image: '/Oleksii.Bolotin.jpg',
     github: 'https://github.com/BolotinAlexey',
     githubName: 'BolotinAlexey',
     linkedIn: 'https://www.linkedin.com/in/oleksii-bolotin/',
   },
   {
-    name: 'Diana Abdykassenova',
-    role: 'Frontend developer',
     image: '/diana.jpg',
     github: 'https://github.com/Diana2886',
     githubName: 'Diana2886',
     linkedIn: 'https://www.linkedin.com/in/diana-abdykassenova/',
   },
   {
-    name: 'Aliaksandr Pranevich',
-    role: 'Frontend developer',
-    image: '',
+    image: '/Aliaksandr.jpeg',
     github: 'https://github.com/freennnn',
     githubName: 'alex.pranevich',
     linkedIn: 'https://www.linkedin.com/in/alexander-pranevich-14597936/',
