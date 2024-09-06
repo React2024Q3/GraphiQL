9c9fe05e604eab447187b335ded2a1b205c33207
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-06 19:38:45 +0500

    feat: add localization to history page

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -5,17 +5,19 @@ import { CommonLinks } from '@/components/ListLinks/CommonLinks';
 import linksStyles from '@/components/ListLinks/ListLinks.module.css';
 import { Loader } from '@/components/Loader';
 import { useAuthRedirect } from '@/shared/hooks/useAuthRedirect';
+import useHistoryLS from '@/shared/hooks/useHistoryLS';
+import urlToRequestTransform from '@/utils/urlToRequestTransform';
 import { Container, Typography } from '@mui/material';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 import styles from './history.module.css';
-import useHistoryLS from '@/shared/hooks/useHistoryLS';
-import urlToRequestTransform from '@/utils/urlToRequestTransform';
 
 export default function History() {
   const { loading, error } = useAuthRedirect();
   // const [history, setHistory] = useState<RequestType[]>([]);
   const [listUrl] = useHistoryLS();
+  const t = useTranslations('history');
   // history.sort((a, b) => b.timestamp - a.timestamp);
 
   // useEffect(() => {
@@ -34,8 +36,8 @@ export default function History() {
       </Typography>
       {!listUrl.length ? (
         <>
-          <Typography variant='subtitle1'>You haven't executed any requests</Typography>
-          <Typography variant='subtitle1'>It's empty here. Try:</Typography>
+          <Typography variant='subtitle1'>{t('empty-1')}</Typography>
+          <Typography variant='subtitle1'>{t('empty-2')}</Typography>
           <ul className={linksStyles.list}>
             <CommonLinks />
           </ul>
@@ -43,14 +45,15 @@ export default function History() {
       ) : (
         listUrl.map((encodeUrl: string, index: number) => {
           const request = urlToRequestTransform(encodeUrl);
-          if(!request) return null;
-          const {url, method} = request;
+          if (!request) return null;
+          const { url, method } = request;
           const route = method === 'GRAPHQL' ? 'graphiql' : 'rest';
-         return (
-          <Link href={route+'/'+encodeUrl} key={index}>
-            {`${method} ${url}`}
-          </Link>
-        )})
+          return (
+            <Link href={route + '/' + encodeUrl} key={index}>
+              {`${method} ${url}`}
+            </Link>
+          );
+        })
       )}
       <ErrorNotification error={error} />
     </Container>
