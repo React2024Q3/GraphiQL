3498c060916b5bc10b0feb7408c5d3820fde74ee
Oleksii Bolotin <samkiral73@gmail.com>
2024-09-12 18:27:08 +0200

    feat: add CodeEditor component

---
+++ src/components/RestForm/CodeEditor/CodeEditor.tsx
@@ -0,0 +1,38 @@
+import { json } from '@codemirror/lang-json';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import CodeMirror from '@uiw/react-codemirror';
+
+interface CodeEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+  isJsonMode?: boolean;
+}
+
+const CodeEditor = ({ value, onChange, isJsonMode = true }: CodeEditorProps) => {
+  const handleBlur = () => {
+    if (isJsonMode) {
+      try {
+        onChange(JSON.stringify(JSON.parse(value), null, 2));
+      } catch (error) {
+        console.error('JSON formatting error: ', error);
+      }
+    }
+  };
+
+  const handleChange = (newValue: string) => {
+    onChange(newValue);
+  };
+
+  return (
+    <CodeMirror
+      value={value}
+      minHeight='150px'
+      theme={vscodeDark}
+      extensions={isJsonMode ? [json()] : []}
+      onChange={handleChange}
+      onBlur={handleBlur}
+    />
+  );
+};
+
+export default CodeEditor;
