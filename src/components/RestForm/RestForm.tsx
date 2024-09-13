0b97f25178861aeffe60ef0da77a4aa6a5b4a2ae
freen <freennnn@gmail.com>
2024-09-13 20:18:35 +0200

    feat: editor variables for graphql synced with history and  rest form implementation

--- src/components/RestForm/RestForm.tsx
+++ src/components/RestForm/RestForm.tsx
@@ -238,7 +238,7 @@ function RestForm({ initMethod, path }: { initMethod: MethodType; path: string[]
               onPairsChange={handlePairsChangeVar}
               title={t('client.variables')}
               initPairs={keyValuePairsVar}
-              isVars={true}
+              shouldSaveToLS={true}
               height={tabIndex === 0 ? '0' : 'none'}
             />
           </Box>
