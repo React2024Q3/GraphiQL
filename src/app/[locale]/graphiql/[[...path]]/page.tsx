2b0c86f7db90e3ced805682b6a92dfc7c566a1a4
freen <freennnn@gmail.com>
2024-09-14 18:05:57 +0200

    feat: page tittle added to GraphiQL page (probably making it client component, not clean server one - due to useTransalte hook)

--- src/app/[locale]/graphiql/[[...path]]/page.tsx
+++ src/app/[locale]/graphiql/[[...path]]/page.tsx
@@ -1,8 +1,13 @@
 import RDTGraphiQLForm from '@/components/GraphiQLForm';
+import { useTranslations } from 'next-intl';
 
 export default function GraphiQLPage({ params }: { params: { path: string[] } }) {
+  const t = useTranslations();
+
   return (
     <>
+      <h2 className='page__title'>{t('buttons.graphql')}</h2>
+
       <RDTGraphiQLForm path={params.path} />
     </>
   );
