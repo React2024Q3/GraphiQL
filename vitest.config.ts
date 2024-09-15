f88f32b65e83ad9b33319ff72049ed1a81123eea
Oleksii Bolotin <samkiral73@gmail.com>
2024-09-15 15:17:58 +0200

    test: add test for RDTGraphiQLForm

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
+    server: {
+      deps: {
+        inline: ['@graphiql/react'],
+      },
+    },
     setupFiles: './src/setupTests.ts',
     coverage: {
       provider: 'v8',
